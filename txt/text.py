f2d9f538168be3d73f0ea12969a426898b2c7568
p4r4dox55 <65310510+p4r4dox55@users.noreply.github.com>
2020-05-13 21:58:44 +0200

    Add files via upload

---
+++ txt/text.py
@@ -0,0 +1,14 @@
+text = [Appuyez sur F pour pirater", "LAISSEZ-MOI ICI", "La sécurité est une illusion", "Profiler ctOS v2.0", "Reprise de DedSec", "Fsociety00.dat", "Evil Corp",
+ "Bonjour, ami", "Le piratage est notre arme", "Bonjour, monde", "Connectez-vous au monde...", "Big Brother vous regarde", "Fuck Society", "Wrench appelle...",
+ Le contrôle est une illusion, installer google_crack.exe..., vous êtes libre ! lol non, c'était une blague, vous êtes un 1 ou un 0, Matraque : 1 - Genou : 0", "Je veux que tu comprennes... Que tu ne sera plus jamais libre...", "Tu pense être intouchable... Je vais briser ton illusion...",
+ "Je veux que tu sache... que tu n'es plus anonyme...", "Snapchat" : T-Bone t'a envoyé un nouveau message...", "LulzSec <3 <3", "<3 Kraken Security OS is bae <3", "DedSec est maintenant dans LinkedIn !
+ FRANCE Champion du monde 2018!!', '~~(8:> est Defalt ~~(8:>', 'Facebook : Neo dans une relation avec Elliot Alderson.', 'Just... fuck the society.', 'locating 127.0.0.1 ... Aucun résultat trouvé',
+ 101100100101100 01100110010110011001", "101100 0110011001", "c2V5cHRvbyBteSBsb3Zl", "1 item dans votre historique web : \'Fkk cuckold how to make your wife a hotwife zootube\'', '49 20 4c 4f 56 45 20 55', 'NB2HI4DTHIXS653XO4XHS33VOR2WEZJOMNXW2L3XMF2GG2B7OY6VUS3OKVZGQYSLJQ3GO====', 'Regarde derrière toi...',
+ dW4gZCdldXggdHJvdWUgw6AgcXUnYSB0J3JlIHNlaW4gcXVlIHNpIGNlIGNldHRl', 'Envoyez-moi des nus : Harry.Truman@nsa.gov", "Le point d'accès "AP-Zone51" a été trouvé à proximité", "┌∩┐(◣_◢)┌∩┐", "Ducky Epstein, occupe les toilettes", "G1gg1L3s /* pipi */", "Ne fais pas de QQ si U ne me trouve pas",
+ "DedSec ? More like", "Mark IV style muthaf*cker !", "U R getting WARMER", "(+) New update is vailable...",
+ "BadBoy17" ? Bad, BOY, 17 ? Vraiment ?", "Itz all 4 d GIGGLES !", "G1gg1L3s... 1L... IL... Compris ?", "Comment tu les trouves ? à² _à²",
+ "Vous devez vous retirer. >_<", "Azarath est une légende/dieu", "Irhel est réel", "<3 Seyptoo <3", "Digidix come back plz !",
+ "Soldat Abdel a écrit une chaussure perdue. Terminé", "Missile Kinder #1 Found !", "cochon maladeeeeeeeee !",
+ "Pornhub and chill", "Alpha : Beta Omega Omega Beta Alpha... IZI", "I am the ONE.", "o_O O.O O.o O_ﾃｳ", "~~(8:> will U be my BFF ? 4 realz !",
+ "'hasta la victoria'", "'Consommez en silence'", "QUELLE VIE PRIVÉE ?", "BADGER BADGER BADGER BADGER", "DAT ASS", "U MAD BRO ?",
+ "HERP DERP HERP DERP", "J'AIME LES TORTUES", "(<3 <3)", "(* *)", "(X X)", "(^ ^)", "(@ @)", "( ! !)", "(~ ~)"]
